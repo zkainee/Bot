169975ada06f3511b5f8d202f8f55e93d52b80cc
Kaine van Ommeren <kainevommeren@gmail.com>
2022-12-12 18:33:53 +0100

    Replaced if statements to switch case
    Better overview

--- src/main/java/nl/kaine/commands/CommandHandler.java
+++ src/main/java/nl/kaine/commands/CommandHandler.java
@@ -8,20 +8,31 @@ public class CommandHandler extends ListenerAdapter {
 
     // Slash Commando's
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+
+        switch (event.getName())
+        {
+            case "assist":
+                Embed.sendAssistReply(event);
+                event.getChannel().
+                break;
+            default:
+                Embed.sendEmbedReply(event);
+        }
+
         if (event.getGuild() == null)
             return;
 
-        if ("assist".equals(event.getName())) {
-            event.deferReply().queue();
-            Embed.sendAssistReply(event);
-        } else if ("report".equals(event.getName())) {
-            event.getHook().sendMessage("We gaan je helpen, hulp nodig? /assist!").setEphemeral(true).queue();
-        } else if ("help".equals(event.getName())) {
-            Embed.sendHelpEmbed(event);
-            event.deferReply().setEphemeral(true).queue();
-        } else {
-            event.deferReply().queue();
-            Embed.sendEmbedReply(event);
-        }
+//        if ("assist".equals(event.getName())) {
+//            event.deferReply().queue();
+//            Embed.sendAssistReply(event);
+//        } else if ("report".equals(event.getName())) {
+//            event.getHook().sendMessage("We gaan je helpen, hulp nodig? /assist!").setEphemeral(true).queue();
+//        } else if ("help".equals(event.getName())) {
+//            Embed.sendHelpEmbed(event);
+//            event.deferReply().setEphemeral(true).queue();
+//        } else {
+//            event.deferReply().queue();
+//            Embed.sendEmbedReply(event);
+//        }
     }
 }
